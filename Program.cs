955789648ead9347b2d8e21d6c01314ccacb8cd0
gregorix <gregorixpd0809@gmail.com>
2020-03-30 23:10:19 -0400

    Prototype Shallow.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Shallow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal() { Nombre = "Chivo", Patas = 4 };
+            Animal animalClonado = animal.Clone() as Animal;
+            animalClonado.Patas = 3;
+
+            Console.WriteLine(animal.Patas);
+
+            Console.ReadKey();
+        }
+    }
+}
